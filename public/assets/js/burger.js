afb41849fcaa4f029a945e20d8f6667caab95461
Haden Liu <haden.liu30@gmail.com>
2020-05-19 09:29:10 +1000

    Add existing file

---
+++ public/assets/js/burger.js
@@ -0,0 +1,48 @@
+$(function() {
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#ca").val().trim(),
+        devoured: 0
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+    $(".eatburger").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var devouredState = {
+            devoured: 1
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(function() {
+            console.log("Burger devoured");
+            location.reload();
+        });
+    });
+
+
+
+
+
+
+  });
